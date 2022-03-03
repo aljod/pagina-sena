3f55ac76190ef1b3355edb607395953103d8163a
aljod <josamontaez@misena.edu.co>
2022-03-03 16:21:24 -0500

    principal page

---
+++ dashboard/tiempo.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('../conexion/conexion.php'); ?>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Tables</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="./estilos .css">
+    <!-- Custom styles for this template -->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+
+                </div>
+                <div class="sidebar-brand-text mx-3"><img src="../images/logosena.png" class="logo"> SENA TIC</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard.html">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+
+
+
+
+
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item ">
+                <a class="nav-link" href="tables.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Aprendices</span></a>
+            </li>
+            <!-- Instructores -->
+            <li class="nav-item active">
+                <a class="nav-link" href="tiempo.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Tiempo instructores
+                    </span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content" class="me-auto">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <form class="form-inline">
+                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </form>
+
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+
+
+
+                        <div class="topbar-divider d-none d-sm-block me-mg"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Settings
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Activity Log
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>C.C</th>
+                                            <th>Nombres</th>
+                                            <th>Apellidos</th>
+                                            <th>Febrero</th>
+                                            <th>Enero</th>
+                                            <th>Acciones</th>
+
+
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>C.C</th>
+                                            <th>Nombres</th>
+                                            <th>Apellidos</th>
+                                            <th>Enero</th>
+                                            <th>Febrero</th>
+                                            <th>Acciones</th>
+
+                                    </tfoot>
+                                    <tbody class="scroll_">
+                                        <?php
+                                        $query = "SELECT * FROM tiempo_instructores";
+                                        $result_tasks = mysqli_query($conn, $query);
+
+                                        while ($row = mysqli_fetch_assoc($result_tasks)) {
+                                        ?>
+                                            <tr>
+                                                <td><?php echo $row['identificacion']; ?></td>
+                                                <td><?php echo $row['nombre']; ?></td>
+                                                <td><?php echo $row['apellido']; ?></td>
+                                                <td><?php echo $row['enero']; ?></td>
+                                                <td><?php echo $row['febrero']; ?></td>
+
+                                                <td>
+                                                    <a href="edit.php?id=<?php echo $row['identificacion'] ?>" class="btn btn-secondary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                        </svg> Editar
+                                                    </a>
+
+                                                </td>
+                                            </tr>
+                                        <?php
+                                        }
+                                        ?>
+
+
+                            </div>
+                            <!-- End of Main Content -->
+
+                            <!-- Instructores de planta -->
+                            <footer class="sticky-footer bg-white">
+                                <div class="container my-auto">
+                                    <div class="copyright text-center my-auto">
+                                        <h1>Instructores sede tic</h1>
+                                    </div>
+                                </div>
+                            </footer>
+                            <!-- End of Footer -->
+
+
+
+
+
+
+
+
+
+
+                           
+                            <!-- Instructores de planta -->
+                            <div class="card shadow mb-4">
+
+                            <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>C.C</th>
+                                                <th>Nombres</th>
+                                                <th>Apellidos</th>
+                                                <th>Febrero</th>
+                                                <th>Enero</th>
+                                                <th>Acciones</th>
+
+
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>C.C</th>
+                                                <th>Nombres</th>
+                                                <th>Apellidos</th>
+                                                <th>Enero</th>
+                                                <th>Febrero</th>
+                                                <th>Acciones</th>
+
+                                        </tfoot>
+                                        <tbody class="scroll_">
+                                            <?php
+                                            $query = "SELECT * FROM tiempo_instructores";
+                                            $result_tasks = mysqli_query($conn, $query);
+
+                                            while ($row = mysqli_fetch_assoc($result_tasks)) {
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $row['identificacion']; ?></td>
+                                                    <td><?php echo $row['nombre']; ?></td>
+                                                    <td><?php echo $row['apellido']; ?></td>
+                                                    <td><?php echo $row['enero']; ?></td>
+                                                    <td><?php echo $row['febrero']; ?></td>
+
+                                                    <td>
+                                                        <a href="edit.php?id=<?php echo $row['identificacion'] ?>" class="btn btn-secondary">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                            </svg> Editar
+                                                        </a>
+
+                                                    </td>
+                                                </tr>
+                                            <?php
+                                            }
+                                            ?>
+
+
+                                </div>
+                                <!-- End of Main Content -->
+
+                                <!-- Footer -->
+                                <footer class="sticky-footer bg-white">
+                                    <div class="container my-auto">
+                                        <div class="copyright text-center my-auto">
+                                            <h1>Instructores De Planta</h1>
+                                        </div>
+                                    </div>
+                                </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                <!-- End of Footer -->
+
+                            </div>
+                            <!-- End of Content Wrapper -->
+
+                        </div>
+                        <!-- End of Page Wrapper -->
+
+                        <!-- Scroll to Top Button-->
+                        <a class="scroll-to-top rounded" href="#page-top">
+                            <i class="fas fa-angle-up"></i>
+                        </a>
+
+                        <!-- Logout Modal-->
+                        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">×</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                        <a class="btn btn-primary" href="login.html">Logout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Bootstrap core JavaScript-->
+                        <script src="vendor/jquery/jquery.min.js"></script>
+                        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+                        <!-- Core plugin JavaScript-->
+                        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+                        <!-- Custom scripts for all pages-->
+                        <script src="js/sb-admin-2.min.js"></script>
+
+                        <!-- Page level plugins -->
+                        <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+                        <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+                        <!-- Page level custom scripts -->
+                        <script src="js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
